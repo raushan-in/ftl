c438ac5cc14ac66487b4bc1ba93c5faea732ceb0
raushan <raushan.in@hotmail.com>
2020-06-01 12:47:39 +0530

    static collection

--- ftl/ftl/settings.py
+++ ftl/ftl/settings.py
@@ -149,3 +149,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+# immaculating static together
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
--- ftl/ftl/urls.py
+++ ftl/ftl/urls.py
@@ -4,6 +4,8 @@ from django.urls import path, include
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework.permissions import AllowAny
+from django.conf import settings
+from django.conf.urls.static import static 
 
 
 # schema view description for swagger
@@ -35,3 +37,6 @@ urlpatterns += [
    path('redoc/', schema_view.with_ui('redoc',
                                       cache_timeout=0), name='schema-redoc'),
 ]
+
+if settings.DEBUG:
+   urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
